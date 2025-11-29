d796c23cf61c737476562ac8104a8628cb70593d
Jakub <56446320+Niewiaro@users.noreply.github.com>
2025-11-30 00:48:08 +0100

    [feat] read sensors data

--- hardware/src/main.cpp
+++ hardware/src/main.cpp
@@ -1,97 +1,263 @@
 #include <Arduino.h>
-#include <WiFi.h>
-#include <PubSubClient.h>
+// #include <WiFi.h>
+// #include <PubSubClient.h>
 
-#include "secrets.h"
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
 
-const char *ssid = WIFI_SSID;
-const char *password = WIFI_PASSWORD;
-const char *mqtt_server = MQTT_SERVER_IP;
+// #include "secrets.h"
 
-const char *topic = "sensor/all";
+#define DEBUG true
+#define FLAME_PIN 12       // flame sensor - digital
+#define GAS_PIN 14         // MQ gas - analog
+#define MOTOR_ADC_PIN 35   // motor voltage measurement
+#define MOTOR_SHUNT_PIN 34 // motor current measurement through shunt resistor
+#define R_SHUNT 47         // shunt resistor in Ohms
 
-WiFiClient espClient;
-PubSubClient client(espClient);
-long lastMsg = 0;
-int value = 0;
+// --- WiFi and MQTT Variables ---
+// const char *ssid = WIFI_SSID;
+// const char *password = WIFI_PASSWORD;
+// const char *mqtt_server = MQTT_SERVER_IP;
 
-void setup_wifi()
-{
-  delay(10);
-  Serial.println();
-  Serial.print("Connecting to WiFi: ");
-  Serial.println(ssid);
+// const char *topic = "sensor/all";
 
-  WiFi.begin(ssid, password);
+// WiFiClient espClient;
+// PubSubClient client(espClient);
+// long lastMsg = 0;
+// int value = 0;
 
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(500);
-    Serial.print(".");
-  }
+// --- MPU6050 Variables ---
+Adafruit_MPU6050 mpu;
+int acceleration_x, acceleration_y, acceleration_z;
+int gyro_x, gyro_y, gyro_z;
+int temperature;
 
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("ESP32 IP Address: ");
-  Serial.println(WiFi.localIP());
-}
+// --- Flame Sensor Variables ---
+bool flame_status = false;
 
-void reconnect()
-{
-  // Loop to reconnect to MQTT
-  while (!client.connected())
-  {
-    Serial.print("Connecting to MQTT...");
-    // Trying to connect with ID "ESP32Client"
-    if (client.connect("ESP32Client"))
-    {
-      Serial.println("connected!");
-    }
-    else
-    {
-      Serial.print("error, rc=");
-      Serial.print(client.state());
-      Serial.println(" trying again in 5 seconds");
-      delay(5000);
-    }
-  }
-}
+// --- Gas Sensor Variables ---
+int gas_level = 0;
+
+// void setup_wifi()
+// {
+//   delay(10);
+
+//   if (DEBUG)
+//   {
+//     Serial.println();
+//     Serial.print("Connecting to WiFi: ");
+//     Serial.println(ssid);
+//   }
+
+//   WiFi.begin(ssid, password);
+
+//   while (WiFi.status() != WL_CONNECTED)
+//   {
+//     delay(500);
+//     if (DEBUG)
+//     {
+//       Serial.print(".");
+//     }
+//   }
+
+//   if (DEBUG)
+//   {
+//     Serial.println("");
+//     Serial.println("WiFi connected");
+//     Serial.println("ESP32 IP Address: ");
+//     Serial.println(WiFi.localIP());
+//   }
+// }
+
+// void reconnect()
+// {
+//   // Loop to reconnect to MQTT
+//   while (!client.connected())
+//   {
+//     if (DEBUG)
+//     {
+//       Serial.print("Connecting to MQTT...");
+//     }
+//     // Trying to connect with ID "ESP32Client"
+//     if (client.connect("ESP32Client"))
+//     {
+//       if (DEBUG)
+//       {
+//         Serial.println("connected!");
+//       }
+//     }
+//     else
+//     {
+//       if (DEBUG)
+//       {
+//         Serial.print("error, rc=");
+//         Serial.print(client.state());
+//         Serial.println(" trying again in 5 seconds");
+//       }
+//       // Wait 5 seconds before retrying
+//       delay(5000);
+//     }
+//   }
+// }
 
 void setup()
 {
-  Serial.begin(115200);
-  setup_wifi();
-  client.setServer(mqtt_server, 1883);
+  if (DEBUG)
+  {
+    Serial.begin(115200);
+    while (!Serial)
+    {
+      delay(10);
+    }
+    Serial.println("Adafruit MPU6050 test!");
+  }
+
+  // setup_wifi();
+  // client.setServer(mqtt_server, 1883);
+
+  // --- MPU6050 Setup ---
+  if (!mpu.begin())
+  {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1)
+    {
+      delay(10);
+    }
+  }
+  if (DEBUG)
+  {
+    Serial.println("MPU6050 Found!");
+  }
+
+  // setupt motion detection
+  mpu.setHighPassFilter(MPU6050_HIGHPASS_0_63_HZ);
+  mpu.setMotionDetectionThreshold(1);
+  mpu.setMotionDetectionDuration(20);
+  mpu.setInterruptPinLatch(true); // Keep it latched.  Will turn off when reinitialized.
+  mpu.setInterruptPinPolarity(true);
+  mpu.setMotionInterrupt(true);
+
+  // Digital sensors
+  pinMode(FLAME_PIN, INPUT);
+
+  // Analog sensors
+  pinMode(GAS_PIN, INPUT);
+  pinMode(MOTOR_ADC_PIN, INPUT);
+  pinMode(MOTOR_SHUNT_PIN, INPUT);
+}
+
+// void loop()
+// {
+//   if (!client.connected())
+//   {
+//     reconnect();
+//   }
+//   client.loop();
+
+//   long now = millis();
+//   if (now - lastMsg > 1000)
+//   {
+//     lastMsg = now;
+//     value++;
+
+//     int temp = 20 + (value % 15);
+//     int flame = value % 2;
+//     int smoke = (value * 5) % 100;
+//     int sound = (value * 3) % 1024;
+//     int vibration = (value * 10) % 50;
+
+//     char msg[256];
+//     snprintf(msg, 256,
+//              "{\"temperature\":%d,\"flame\":%d,\"smoke\":%d,\"sound\":%d,\"vibration\":%d}",
+//              temp, flame, smoke, sound, vibration);
+
+//     if (DEBUG)
+//     {
+//       Serial.print("Sending JSON: ");
+//       Serial.println(msg);
+//     }
+
+//     client.publish(topic, msg);
+//   }
+// }
+
+void get_mpu_data()
+{
+  /* Get new sensor events with the readings */
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+
+  acceleration_x = a.acceleration.x;
+  acceleration_y = a.acceleration.y;
+  acceleration_z = a.acceleration.z;
+
+  gyro_x = g.gyro.x;
+  gyro_y = g.gyro.y;
+  gyro_z = g.gyro.z;
+
+  temperature = temp.temperature;
+
+  if (DEBUG)
+  {
+    Serial.print("Acceleration X: ");
+    Serial.print(acceleration_x);
+    Serial.print(", Y: ");
+    Serial.print(acceleration_y);
+    Serial.print(", Z: ");
+    Serial.print(acceleration_z);
+    Serial.println(" m/s^2");
+
+    Serial.print("Rotation X: ");
+    Serial.print(gyro_x);
+    Serial.print(", Y: ");
+    Serial.print(gyro_y);
+    Serial.print(", Z: ");
+    Serial.print(gyro_z);
+    Serial.println(" rad/s");
+
+    Serial.print("Temperature: ");
+    Serial.print(temperature);
+    Serial.println(" degC");
+    Serial.println("");
+  }
+}
+
+void get_flame_data()
+{
+  flame_status = (digitalRead(FLAME_PIN) != HIGH); // high means no flame
+
+  if (DEBUG)
+  {
+    if (flame_status)
+    {
+      Serial.println("Safe: No flame detected.");
+    }
+    else
+    {
+      Serial.println("🔥🔥🔥 FIRE DETECTED! 🔥🔥🔥");
+    }
+  }
+}
+
+void get_gas_data()
+{
+  gas_level = analogRead(GAS_PIN); // 0–4095 na ESP32
+  if (DEBUG)
+  {
+    Serial.print("Gas Level: ");
+    Serial.println(gas_level);
+  }
 }
 
 void loop()
 {
-  if (!client.connected())
+  if (mpu.getMotionInterruptStatus())
   {
-    reconnect();
+    get_mpu_data();
   }
-  client.loop();
 
-  long now = millis();
-  if (now - lastMsg > 2000)
-  {
-    lastMsg = now;
-    value++;
-
-    int temp = 20 + (value % 15);
-    int flame = value % 2;
-    int smoke = (value * 5) % 100;
-    int sound = (value * 3) % 1024;
-    int vibration = (value * 10) % 50;
-
-    char msg[256];
-    snprintf(msg, 256,
-             "{\"temperature\":%d,\"flame\":%d,\"smoke\":%d,\"sound\":%d,\"vibration\":%d}",
-             temp, flame, smoke, sound, vibration);
-
-    Serial.print("Sending JSON: ");
-    Serial.println(msg);
-
-    client.publish(topic, msg);
-  }
+  get_flame_data();
+  get_gas_data();
+  delay(1000);
 }
